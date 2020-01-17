fc3158718de408c605f1ae825580be413e9f8e0e
Jonas Pedersen <jonas@pedersen.ninja>
2020-01-17 22:18:46 +0100

    Basic parsing of time series data.

--- pyeloverblik/__main__.py
+++ pyeloverblik/__main__.py
@@ -10,11 +10,11 @@ def main():
     '''
     parser = argparse.ArgumentParser("pydanfossair")
     parser.add_argument("--refresh-token", action="store", required=True)
-    parser.add_argument('--meetering-point', action='store', required=True)
+    parser.add_argument('--metering-point', action='store', required=True)
     
     args = parser.parse_args()
 
-    eloverblik.Eloverblik(args.refresh_token).getTimeSeries(args.meetering_point)
+    print(eloverblik.Eloverblik(args.refresh_token).getYesterDayNiceFormat(args.metering_point))
 
 if __name__ == "__main__":
     main()
--- pyeloverblik/eloverblik.py
+++ pyeloverblik/eloverblik.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from datetime import timedelta
 import requests
+import json
 
 class Eloverblik:
     def __init__(self, refreshToken):
@@ -9,6 +10,9 @@ class Eloverblik:
         pass
 
     def getTimeSeries(self, meetering_point, fromDate=datetime.now()-timedelta(days=1), toDate=datetime.now(), aggregation='Hour'):
+        '''
+        Call time series API on eloverblik.dk. Defaults to yester days data. 
+        '''
         url = self._base_url + 'api/Token'
         print(url)
         headers = {'Authorization': 'Bearer ' + self._refresh_token}
@@ -23,15 +27,24 @@ class Eloverblik:
         f = fromDate.strftime(dateFormat)
         t = toDate.strftime(dateFormat)
         body = "{\"meteringPoints\": {\"meteringPoint\": [\"" + meetering_point + "\"]}}"
+        
         headers = {'Authorization': 'Bearer ' + short_token, 'Content-Type': 'application/json', 'Accept': 'application/json'}
+        
         response = requests.post(self._base_url + f'/api/MeterData/GetTimeSeries/{f}/{t}/Hour', data=body, headers=headers)
         
         response.raise_for_status()
 
-        print(response.json())
-
         return response.json()
 
+    def getYesterDayNiceFormat(self, metering_point):
+        raw_result = self.getTimeSeries(metering_point)
+
+        response = dict()
+
+        for k in raw_result['result'][0]['MyEnergyData_MarketDocument']['TimeSeries'][0]['Period'][0]['Point']:
+            response[k['position']] = k['out_Quantity.quantity']
+
+        return response
 
 
 
