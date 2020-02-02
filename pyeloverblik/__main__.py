86a6763b930f5cc49f4a906b01e9343d882ea17e
Jonas Pedersen <jonas@pedersen.ninja>
2020-02-02 13:43:10 +0100

    Only print result if successful.

--- pyeloverblik/__main__.py
+++ pyeloverblik/__main__.py
@@ -11,18 +11,21 @@ def main():
     parser = argparse.ArgumentParser("pydanfossair")
     parser.add_argument("--refresh-token", action="store", required=True)
     parser.add_argument('--metering-point', action='store', required=True)
-    
+
     args = parser.parse_args()
 
     result = Eloverblik(args.refresh_token).get_yesterday_parsed(args.metering_point)
+    if result.status == 200:
+        total = 0
+        print(f"Date: {result.data_date}")
+        for hour in range(24):
+            data = result.get_metering_data(hour)
+            total += data
+            print(f"Hour {hour}-{hour+1}: {data}kWh")
 
-    total = 0
-    print(f"Date: {result.data_date}")
-    for x in range(24):
-        data = result.get_metering_data(x)
-        total += data
-        print(f"Hour {x}-{x+1}: {data}kWh")
-    
-    print(f"Total: {total}kWh")
+        print(f"Total: {total}kWh")
+    else:
+        print(f"Error getting data. Status: {result.status}. Error: {result.detailed_status}")
+
 if __name__ == "__main__":
     main()
